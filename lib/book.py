a20883c341b5390bdff67a1729770f2703771760
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-07-21 15:41:14 -0400

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def get_title(self):
+        return self._title
+
+    def set_title(self, value):
+        self._title = value
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    title = property(get_title, set_title)
+    page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self._condition = "New"
+
+    def get_brand(self):
+        return self._brand
+
+    def set_brand(self, value):
+        self._brand = value
+
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    size = property(get_size, set_size)
+
+    def get_condition(self):
+        return self._condition
+
+    def set_condition(self, value):
+        self._condition = value
+
+    condition = property(get_condition, set_condition)
+
+    def cobble(self):
+        self._condition = "New"
+        print("Your shoe is as good as new!")
